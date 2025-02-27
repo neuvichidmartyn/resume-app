2a14f6641ef8aa2e12de5b9ea0b5e8daa3a1887b
neuvichidmartyn <deemartynov@gmail.com>
2025-02-27 17:08:48 +0300

    пути в билд

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,19 @@
 import './footer.css';
+import resumeData from '../data/resumeData.js';
 
 function Footer() {
   return (
     <footer className="footer">
       <p>© {new Date().getFullYear()} neuvichidmartyn. Все права защищены.</p>
+      <ul className="contacts_list">
+            {resumeData.contacts.map((contact, index) => (
+              <li key={index} className="contacts_item">
+                <a href={contact.link} className="contacts_link" target="_blank" rel="noopener noreferrer">
+                  {contact.type}: {contact.value}
+                </a>
+              </li>
+            ))}
+          </ul>
     </footer>
   );
 }
